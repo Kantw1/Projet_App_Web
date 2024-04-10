8fb7773c665e54cc2c4bc5bfbbfc6540849c52eb
Kantw1 <fourrierquentin9@gmail.com>
2024-04-10 23:17:03 +0200

    test11 ajout des id dans la table user_agendas

--- signup.php
+++ signup.php
@@ -32,14 +32,12 @@ if ($conn->query($sql) === TRUE) {
     // Requête SQL pour insérer l'entrée dans la table user_agendas
     $sql_insert_user_agenda = "INSERT INTO user_agendas (user_id) VALUES ('$user_id')";
 
-    if ($con->query($sql_insert_user_agenda) === TRUE) {
+    if ($conn->query($sql_insert_user_agenda) === TRUE) {
         $_SESSION['user_id_agenda'] = $user_id;
         header('Location: index.html');
     } else {
         header('Location: signup.html');
     }
-
-    
 } else {
     header('Location: signup.html');
 }
