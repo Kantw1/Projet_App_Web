89ff713bf54145a5a2b911956e2f24e98684449c
Kantw1 <fourrierquentin9@gmail.com>
2024-04-11 14:50:20 +0200

    test8 connection a agendas_user après la création d'un agenda

--- connection_agenda_user.php
+++ connection_agenda_user.php
@@ -37,21 +37,21 @@ if(isset($_SESSION['user_id'])) {
         }
 
         // Mettre à jour la ligne existante avec le nouveau agenda_code
-        $sql_update_user_agenda = "UPDATE user_agendas SET agenda_code = '$new_agenda_code' WHERE user_id = '$user_id'";
+        $sql_update_user_agenda = "UPDATE user_agenda SET agenda_code = '$new_agenda_code' WHERE user_id = '$user_id'";
 
         if ($conn->query($sql_update_user_agenda) === TRUE) {
-            echo "Code d'agenda mis à jour avec succès dans la table user_agendas";
+            echo "Code d'agenda mis à jour avec succès dans la table user_agenda";
         } else {
-            echo "Erreur lors de la mise à jour du code d'agenda dans la table user_agendas : " . $conn->error;
+            echo "Erreur lors de la mise à jour du code d'agenda dans la table user_agenda : " . $conn->error;
         }
     } else {
         // Aucune ligne trouvée pour cet utilisateur, insérer un nouveau enregistrement
-        $sql_insert_user_agenda = "INSERT INTO user_agendas (user_id, agenda_code) VALUES ('$user_id', '$agenda_code')";
+        $sql_insert_user_agenda = "INSERT INTO user_agenda (user_id, agenda_code) VALUES ('$user_id', '$agenda_code')";
 
-        if ($conn->query($sql_insert_user_agenda) === TRUE) {
-            echo "Nouveau code d'agenda ajouté avec succès dans la table user_agendas";
+        if ($conn->query($sql_insert_user_agenda) === TRUE) {user_agenda
+            echo "Nouveau code d'agenda ajouté avec succès dans la table user_agenda";
         } else {
-            echo "Erreur lors de l'ajout du nouveau code d'agenda dans la table user_agendas : " . $conn->error;
+            echo "Erreur lors de l'ajout du nouveau code d'agenda dans la table user_agenda : " . $conn->error;
         }
     }
 } else {
