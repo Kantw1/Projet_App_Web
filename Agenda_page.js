5f3fe21f1e9f41118e0791ad6c5f65b828c66ac8
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-04-25 00:12:48 +0200

    test ajout php list_agenda18

--- Agenda_page.js
+++ Agenda_page.js
@@ -59,7 +59,7 @@ const months = [
 
 const eventsArr = [];
 getEvents();
-//console.log(eventsArr);
+console.log(eventsArr);
 
 //function to add days in days with class day and prev-date next-date on previous month and next month days and active on today
 function initCalendar() {
--- Liste_agenda.js
+++ Liste_agenda.js
@@ -3,8 +3,6 @@ function getAgendaData() {
     fetch('Liste_agenda.php') // Envoyer une requête HTTP à Liste_agenda.php pour récupérer les données des agendas
     .then(response => response.json()) // Convertir la réponse en JSON
     .then(Data => {
-        // Afficher les données des agendas dans la console
-        console.log("Données des agendas récupérées avec succès : ");
         // Stocker les données des agendas dans une constante
         const agendaData = Data;
         // Appeler la fonction pour afficher les données des agendas
