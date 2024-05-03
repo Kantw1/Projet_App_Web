4c3df80b06865dd3de672886906b0380100609f8
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-03 14:01:35 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,4 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-// Inclure PHPMailer
-require 'vendor/autoload.php';
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Connexion à la base de données
 $servername = "localhost:3306"; // Ou l'adresse de votre serveur SQL
 $username = "cycalguj";
@@ -28,7 +18,6 @@ session_start();
 // Récupération des données du formulaire
 $username = $_POST['username'];
 $password = $_POST['password'];
-$code = $_POST['code']; // Nouveau champ pour le code d'authentification
 
 // Requête SQL pour vérifier les informations d'identification
 $sql = "SELECT id, first_name, last_name FROM users WHERE username='$username' AND password='$password'";
@@ -38,38 +27,14 @@ if ($result->num_rows > 0) {
     // Récupération des données de nom et prénom de l'utilisateur
     $row = $result->fetch_assoc();
     
-    // Générer un code d'authentification aléatoire
-    $code = mt_rand(100000, 999999);
+    // Stockage de l'identifiant de l'utilisateur dans une session
+    $_SESSION['username'] = $username;
+    $_SESSION['first_name'] = $row['first_name'];
+    $_SESSION['last_name'] = $row['last_name'];
+    $_SESSION['user_id'] = $row['id']; // Stockage de l'ID de l'utilisateur
     
-    // Enregistrer le code dans la session
-    $_SESSION['auth_code'] = $code;
-    
-    // Envoyer le code par e-mail
-    $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'cycalender.site'; // Remplacez par le nom d'hôte SMTP de votre serveur
-        $mail->SMTPAuth = true;
-        $mail->Username = 'code.authentification@cycalender.site'; // Votre adresse e-mail
-        $mail->Password = 'CYCalender1234'; // Votre mot de passe
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 465; // Port SMTP
-        
-        $mail->setFrom('code.authentification@cycalender.site', 'CYCalender');
-        $mail->addAddress($_POST['username']);
-        
-        $mail->isHTML(true);
-        $mail->Subject = 'Code d\'authentification';
-        $mail->Body    = 'Votre code d\'authentification est : ' . $code;
-        
-        $mail->send();
-        echo 'Un e-mail contenant le code d\'authentification a été envoyé.';
-    } catch (Exception $e) {
-        echo 'Erreur lors de l\'envoi de l\'e-mail : ', $mail->ErrorInfo;
-    }
-
-    // Redirection vers la vérification du code
-    header('Location: verification_code.php');
+    // Redirection vers l'agenda collaboratif
+    header('Location: Agenda.html');
     exit;
 } else {
     echo "Identifiants incorrects";
