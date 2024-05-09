4d2c830569758cd00dc5302fdbfbdfaf80643779
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-09 21:15:53 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -46,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $uniqueCode = generateUniqueCode();
 
         // Insertion de l'événement dans la base de données
-        $sql_insert = "INSERT INTO events (day, month, year, title, time, description, place, creator, code_agenda) VALUES ('$event_start_time', '$event_end_time', '$event_name', '$event_description', '$event_place', '$event_creator', '$uniqueCode')";
+        $sql_insert = "INSERT INTO events (day, month, year, title, start_time, end_time, description, place, creator, code_agenda) VALUES ('$event_start_time', '$event_end_time', '$event_name', '$event_description', '$event_place', '$event_creator', '$uniqueCode')";
 
         if ($conn->query($sql_insert) === TRUE) {
             $alert_message = "Nouvel événement créé avec succès avec le code : " . $uniqueCode;
