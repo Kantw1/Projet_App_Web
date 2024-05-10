d94347ffa68d4251a279293e27a4fa5c983b6d31
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-10 02:44:55 +0200

    ajout agenda5

--- Add_agenda.js
+++ Add_agenda.js
@@ -1,7 +1,7 @@
 const Agenda_code = document.querySelector(".agenda-code"),
    Add_agenda = document.querySelector(".add-agenda");
 
-   Create_agenda.addEventListener("click", () => {
+   Add_agenda.addEventListener("click", () => {
     const AgendaCode = Agenda_code.value;
     alert("test");
 
