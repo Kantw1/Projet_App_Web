76a5a447dd4dbf47a1f3c5f25bcaf947d201a162
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-10 17:50:40 +0200

    ajout agenda test19

--- Add_agenda.php
+++ Add_agenda.php
@@ -36,20 +36,23 @@ if (isset($_POST["Agenda_Code"])) {
                 if (in_array($agenda_code, $agenda_codes)) {
                     $message = "Agenda déjà acquis";
                 } else {
+                    $message = "";
+                    echo json_encode(array("message" => $message));
+
                     $_SESSION['agenda_code'] = $agenda_code;
-                    //include 'connection_agenda_user.php';
+                    include 'connection_agenda_user.php';
                 }
             }
         } else {
             // Si le code n'existe pas, envoyer un message "Agenda non trouvé"
             $message = "Agenda non trouvé";
+            echo json_encode(array("message" => $message));
         }
     } else {
         // Si l'utilisateur n'est pas connecté, renvoyer un message d'erreur
         $message = "Utilisateur non connecté";
+        echo json_encode(array("message" => $message));
     }
-
-    echo json_encode(array("message" => $message));
 }
 
 $conn->close();
