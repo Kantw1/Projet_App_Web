4ffcc3849531fe46c92ef155a487d797be0cfc9b
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-10 16:46:29 +0200

    ajout agenda test4

--- Add_agenda.php
+++ Add_agenda.php
@@ -21,8 +21,18 @@ if (isset($_POST["Agenda_Code"])) {
 $sql_check = "SELECT id FROM agendas WHERE agenda_code = '$agenda_code'";
 $result_check = $conn->query($sql_check);
 if ($result_check->num_rows > 0) {
-    // Si le code existe afficher une alerte : agenda trouvé
-    $message = "Agenda trouvé";
+
+    // Requête pour vérifier si le code généré est présent dans la base de données
+    $sql_check = "SELECT * FROM user_agenda WHERE FIND_IN_SET('$agenda_code', agenda_code)";
+    $result_check = $conn->query($sql_check);
+    if ($result_check->num_rows > 0) {
+        // Si le code existe, envoyer un message "Agenda trouvé"
+        $message = array("message" => "Agenda déjà aquis");
+    } else {
+        // Si le code n'existe pas, envoyer un message "Agenda non trouvé"
+        $message = array("message" => "Ajout de l'agenda ");
+    }
+
 } else {
     //afficher une alerte disant agenda non trouvé
     $message = "Agenda non trouvé";
