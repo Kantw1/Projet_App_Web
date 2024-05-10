531a6c58d3ff2a18363b5940bb651ccb81db6f7e
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-10 02:00:15 +0200

    creation agenda nouvel essaie33

--- Create_Agenda.js
+++ Create_Agenda.js
@@ -19,7 +19,7 @@ const Agenda_name = document.querySelector(".Agenda_name"),
         .then(response => response.text())
             .then(data => {
                 //alert(data);
-                getAgendaDatas();
+                getAgendaData_ajout();
             })
         .catch(error => console.error('Erreur lors de la creation de l\'agenda:', error));
    });
