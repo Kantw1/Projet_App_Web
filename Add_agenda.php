0acf032247432497a194a7f38f216347aece9136
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-10 16:57:01 +0200

    ajout agenda test5

--- Add_agenda.php
+++ Add_agenda.php
@@ -17,29 +17,32 @@ if (isset($_POST["Agenda_Code"])) {
     // Récupération du code de l'agenda depuis le formulaire
     $agenda_code = $_POST['Agenda_Code'];
 
-    // Vérification si le code généré est présent dans la base de données
-$sql_check = "SELECT id FROM agendas WHERE agenda_code = '$agenda_code'";
-$result_check = $conn->query($sql_check);
-if ($result_check->num_rows > 0) {
-
     // Requête pour vérifier si le code généré est présent dans la base de données
-    $sql_check = "SELECT * FROM user_agenda WHERE FIND_IN_SET('$agenda_code', agenda_code)";
+    $sql_check = "SELECT id FROM agendas WHERE agenda_code = '$agenda_code'";
     $result_check = $conn->query($sql_check);
     if ($result_check->num_rows > 0) {
-        // Si le code existe, envoyer un message "Agenda trouvé"
-        $message = array("message" => "Agenda déjà aquis");
+        // Récupérer les codes des agendas associés à l'utilisateur
+        $sql_user_agenda = "SELECT agenda_code FROM user_agenda WHERE user_id = '$user_id'";
+        $result_user_agenda = $conn->query($sql_user_agenda);
+        if ($result_user_agenda->num_rows > 0) {
+            $row = $result_user_agenda->fetch_assoc();
+            $agenda_codes = explode(",", $row["agenda_code"]);
+            //vérifier si agenda_code appartient au tableau agenda_codes
+            if (in_array($agenda_code, $agenda_codes)) {
+                $message = "Agenda déjà acquis";
+            } else {
+                $message = "Ajout de l'agenda";
+            }
+        } else {
+            // Si l'utilisateur n'a pas d'agenda, ajoutez simplement celui-ci
+            $message = "Ajout de l'agenda";
+        }
     } else {
         // Si le code n'existe pas, envoyer un message "Agenda non trouvé"
-        $message = array("message" => "Ajout de l'agenda ");
+        $message = "Agenda non trouvé";
     }
 
-} else {
-    //afficher une alerte disant agenda non trouvé
-    $message = "Agenda non trouvé";
-}
-
-echo json_encode(array("message" => $message));
-
+    echo json_encode(array("message" => $message));
 }
 
 $conn->close();
