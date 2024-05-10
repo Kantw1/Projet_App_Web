214f9c9ac4f5f5439a86c2093538e31683776440
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-10 17:41:31 +0200

    ajout agenda test15

--- Add_agenda.js
+++ Add_agenda.js
@@ -19,11 +19,10 @@ Add_agenda.addEventListener("click", () => {
         if(data.message) {
             alert(data.message); // Afficher le message de réponse
             return;
-        } else {
-            getAgendaData_ajout();
-            var nav = document.querySelector('.new-agenda');
-            nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
         }
+        getAgendaData_ajout();
+        var nav = document.querySelector('.new-agenda');
+        nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
     })
     .catch(error => console.error('Erreur lors de la création de l\'agenda:', error));    
 
