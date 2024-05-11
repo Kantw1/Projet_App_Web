120a98b0b956818e2e59d32fea52fb0cfa52c70e
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-12 00:05:03 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,31 +4,40 @@ session_start();
 // Vérifier si la variable de session agenda_code est définie et égale à "J12VC4I"
 if(isset($_SESSION['agenda_code']) && $_SESSION['agenda_code'] == "J12VC4I") {
     // Tableau de données de test
-    $donnees_test = array(
-        array(
-            "day" => "11",
-            "month" => "5",
-            "year" => "2024",
-            "title" => "Événement 1",
-            "event_time" => "12h45 - 12h46",
-            "description" => "Description de l'événement 1",
-            "place" => "Lieu de l'événement 1",
-            "creator" => "Créateur de l'événement 1",
-            "code_agenda" => "J12VC4I"
-        ),
-        array(
-            "day" => "12",
-            "month" => "5",
-            "year" => "2024",
-            "title" => "Événement 2",
-            "event_time" => "12h55 - 15h46",
-            "description" => "Description de l'événement 2",
-            "place" => "Lieu de l'événement 2",
-            "creator" => "Créateur de l'événement 2",
-            "code_agenda" => "J12VC4I"
-        ),
-        // Ajoutez plus de données au besoin
-    );
+    $donnees_test = [
+        [
+            "day" => 1,
+            "month" => 5,
+            "year" => 2024,
+            "events" => [
+                [
+                    "title" => "Réunion importante",
+                    "time" => "10h00 - 11h30",
+                    "description" => "Réunion de lancement du nouveau projet",
+                    "place" => "Salle de conférence",
+                ],
+                [
+                    "title" => "Déjeuner avec l'équipe",
+                    "time" => "12h30 - 14h00",
+                    "description" => "Déjeuner avec les membres de l'équipe pour discuter des prochaines étapes",
+                    "place" => "Restaurant XYZ",
+                ],
+            ],
+        ],
+        [
+            "day" => 5,
+            "month" => 5,
+            "year" => 2024,
+            "events" => [
+                [
+                    "title" => "Entretien d'évaluation",
+                    "time" => "9h00 - 10h00",
+                    "description" => "Entretien d'évaluation semestriel",
+                    "place" => "Bureau",
+                ],
+            ],
+        ],
+    ];
 
     // Convertir le tableau en JSON
     $donnees_json = json_encode($donnees_test);
@@ -40,4 +49,5 @@ if(isset($_SESSION['agenda_code']) && $_SESSION['agenda_code'] == "J12VC4I") {
     echo "Accès non autorisé";
 }
 ?>
+
 
