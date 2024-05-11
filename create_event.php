9e3f849bd4b14c2f77a64506bcb54e8980d74a5d
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-11 21:45:29 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -15,21 +15,28 @@ $data = json_decode(file_get_contents("php://input"), true);
 session_start();
 
 foreach ($data as $event) {
-    $day = $event['day'];
-    $month = $event['month'];
-    $year = $event['year'];
-    $title = $event['title'];
-    $time = $event['time'];
-    $event_time = date("H:i", strtotime($time));
-    $description = isset($event['description']) ? $event['description'] : '';
-    $place = isset($event['place']) ? $event['place'] : '';
-    $creator = $_SESSION['username'];
-    $code_agenda = $_SESSION['agenda_code'];
+    // Vérifier si toutes les clés sont présentes
+    if (isset($event['day'], $event['month'], $event['year'], $event['title'], $event['time'])) {
+        $day = intval($event['day']); // Convertir en entier
+        $month = intval($event['month']);
+        $year = intval($event['year']);
+        $title = $event['title'];
+        $time = $event['time'];
+        $event_time = date("H:i", strtotime($time));
+        $description = isset($event['description']) ? $event['description'] : '';
+        $place = isset($event['place']) ? $event['place'] : '';
+        $creator = $_SESSION['username'];
+        $code_agenda = $_SESSION['agenda_code'];
 
-    $sql_insert = "INSERT INTO events (day, month, year, title, event_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql_insert);
-    $stmt->bind_param("iiissssss", $day, $month, $year, $title, $event_time, $description, $place, $creator, $code_agenda);
-    $stmt->execute();
+        // Insertion de l'événement dans la table d'événements
+        $sql_insert = "INSERT INTO events (day, month, year, title, event_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bind_param("iiissssss", $day, $month, $year, $title, $event_time, $description, $place, $creator, $code_agenda);
+        $stmt->execute();
+    } else {
+        // Afficher un message d'erreur si des clés sont manquantes
+        echo json_encode(array("error" => "Certaines clés sont manquantes dans les données d'événement"));
+    }
 }
 
 $conn->close();
