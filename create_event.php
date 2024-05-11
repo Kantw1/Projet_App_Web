6f862236d1877bdf29a48c97ecb642969a347bfd
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-11 21:39:04 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -1,5 +1,4 @@
 <?php
-// Connexion à la base de données
 $servername = "localhost";
 $username = "cycalguj";
 $password = "CYCalender1234";
@@ -7,44 +6,35 @@ $dbname = "CYCalenderB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérification de la connexion
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Échec de la connexion: " . $conn->connect_error);
 }
 
-// Récupération des données envoyées en JSON
 $data = json_decode(file_get_contents("php://input"), true);
 
 session_start();
 
-// Traitement des données et insertion dans la base de données
 foreach ($data as $event) {
     $day = $event['day'];
     $month = $event['month'];
     $year = $event['year'];
     $title = $event['title'];
-
-    // Récupération de l'heure de l'événement
     $time = $event['time'];
     $event_time = date("H:i", strtotime($time));
+    $description = isset($event['description']) ? $event['description'] : '';
+    $place = isset($event['place']) ? $event['place'] : '';
+    $creator = $_SESSION['username'];
+    $code_agenda = $_SESSION['agenda_code'];
 
-    $description = isset($event['description']) ? $event['description'] : ''; // Vérification de la description
-    $place = isset($event['place']) ? $event['place'] : ''; // Vérification du lieu
-    $creator = $_SESSION['username']; // Ajustez selon votre système d'authentification
-    $code_agenda = $_SESSION['agenda_code']; // Ajustez selon votre système d'authentification
-
-    // Insertion de l'événement dans la table d'événements
     $sql_insert = "INSERT INTO events (day, month, year, title, event_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql_insert);
     $stmt->bind_param("iiissssss", $day, $month, $year, $title, $event_time, $description, $place, $creator, $code_agenda);
     $stmt->execute();
 }
 
-// Fermeture de la connexion
 $conn->close();
 
-// Répondre avec succès
-echo json_encode(array("message" => "Events saved successfully"));
+echo json_encode(array("message" => "Événements enregistrés avec succès"));
 ?>
 
 
