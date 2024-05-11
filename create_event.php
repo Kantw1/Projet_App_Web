aa9262cbf87844ad977a4735e5baa1756f2803d7
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-11 21:57:39 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servername = "localhost";
 $username = "cycalguj";
 $password = "CYCalender1234";
@@ -15,9 +19,9 @@ $data = json_decode(file_get_contents("php://input"), true);
 session_start();
 
 foreach ($data as $event_data) {
-    $day = $event_data['day'];
-    $month = $event_data['month'];
-    $year = $event_data['year'];
+    $day = intval($event_data['day']); // Convertir en entier
+    $month = intval($event_data['month']);
+    $year = intval($event_data['year']);
     $events = $event_data['events'];
 
     foreach ($events as $event) {
@@ -26,8 +30,8 @@ foreach ($data as $event_data) {
         $event_time = date("H:i", strtotime($time));
         $description = isset($event['description']) ? $event['description'] : '';
         $place = isset($event['place']) ? $event['place'] : '';
-        $creator = $_SESSION['username'];
-        $code_agenda = $_SESSION['agenda_code'];
+        $creator = isset($_SESSION['username']) ? $_SESSION['username'] : ''; // Vérifier si la clé 'username' est définie
+        $code_agenda = isset($_SESSION['agenda_code']) ? $_SESSION['agenda_code'] : ''; // Vérifier si la clé 'agenda_code' est définie
 
         $sql_insert = "INSERT INTO events (day, month, year, title, event_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $stmt = $conn->prepare($sql_insert);
@@ -40,3 +44,4 @@ $conn->close();
 
 echo json_encode(array("message" => "Événements enregistrés avec succès"));
 ?>
+
