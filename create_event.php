0a5a52f0e5f844c33fedddaa2227dbee342a10c0
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-11 21:24:05 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -24,11 +24,10 @@ foreach ($data as $event) {
     $year = $event['year'];
     $title = $event['title'];
 
-    // Récupération des heures de début et de fin à partir de la chaîne de caractères
+    // Récupération de l'heure à partir de la chaîne de caractères
     $time = $event['time'];
     $times = explode(" - ", $time);
-    $start_time = date("H:i", strtotime($times[0])); // Heure de début
-    $end_time = date("H:i", strtotime($times[1])); // Heure de fin
+    $event_time = date("H:i", strtotime($times[0])); // Heure de l'événement
 
     $description = isset($event['description']) ? $event['description'] : ''; // Vérification de la description
     $place = isset($event['place']) ? $event['place'] : ''; // Vérification du lieu
@@ -36,9 +35,9 @@ foreach ($data as $event) {
     $code_agenda = $_SESSION['agenda_code']; // Ajustez selon votre système d'authentification
 
     // Insertion de l'événement dans la table d'événements
-    $sql_insert = "INSERT INTO events (day, month, year, title, start_time, end_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql_insert = "INSERT INTO events (day, month, year, title, event_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql_insert);
-    $stmt->bind_param("iissssssss", $day, $month, $year, $title, $start_time, $end_time, $description, $place, $creator, $code_agenda);
+    $stmt->bind_param("iisssssss", $day, $month, $year, $title, $event_time, $description, $place, $creator, $code_agenda);
     $stmt->execute();
 }
 
@@ -48,6 +47,5 @@ $conn->close();
 // Répondre avec succès
 echo json_encode(array("message" => "Events saved successfully"));
 ?>
-
 
 
