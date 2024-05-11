52e7ac99c455b7ff9129d5632641ad136cc5fc85
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-11 21:02:26 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -19,22 +19,27 @@ session_start();
 
 // Traitement des données et insertion dans la base de données
 foreach ($data as $event) {
-    $date = $event['year'] . "-" . $event['month'] . "-" . $event['day'];
-    foreach ($event['events'] as $subEvent) {
-        $title = $subEvent['title'];
-        $start_time = $subEvent['start_time']; // Heure de début
-        $end_time = $subEvent['end_time']; // Heure de fin
-        $description = isset($subEvent['description']) ? $subEvent['description'] : ''; // Vérification de la description
-        $place = isset($subEvent['place']) ? $subEvent['place'] : ''; // Vérification du lieu
-        $creator = $_SESSION['username']; // Ajustez selon votre système d'authentification
-        $code_agenda = $_SESSION['agenda_code'];; // Ajustez selon votre système d'authentification
+    $day = $event['day'];
+    $month = $event['month'];
+    $year = $event['year'];
+    $title = $event['title'];
 
-        // Insertion de l'événement dans la table d'événements
-        $sql_insert = "INSERT INTO events (day, month, year, title, start_time, end_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bind_param("iiisssssss", $event['day'], $event['month'], $event['year'], $title, $start_time, $end_time, $description, $place, $creator, $code_agenda);
-        $stmt->execute();
-    }
+    // Récupération des heures de début et de fin à partir de la chaîne de caractères
+    $time = $event['time'];
+    $times = explode(" - ", $time);
+    $start_time = date("H:i", strtotime($times[0])); // Heure de début
+    $end_time = date("H:i", strtotime($times[1])); // Heure de fin
+
+    $description = isset($event['description']) ? $event['description'] : ''; // Vérification de la description
+    $place = isset($event['place']) ? $event['place'] : ''; // Vérification du lieu
+    $creator = $_SESSION['username']; // Ajustez selon votre système d'authentification
+    $code_agenda = $_SESSION['agenda_code']; // Ajustez selon votre système d'authentification
+
+    // Insertion de l'événement dans la table d'événements
+    $sql_insert = "INSERT INTO events (day, month, year, title, start_time, end_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bind_param("iissssssss", $day, $month, $year, $title, $start_time, $end_time, $description, $place, $creator, $code_agenda);
+    $stmt->execute();
 }
 
 // Fermeture de la connexion
@@ -43,5 +48,6 @@ $conn->close();
 // Répondre avec succès
 echo json_encode(array("message" => "Events saved successfully"));
 ?>
+
 
 
