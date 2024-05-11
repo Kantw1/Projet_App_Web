2f718a9ec38558d6504fa976413e42957654dd66
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-11 22:08:52 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -3,45 +3,41 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
+// Connexion à la base de données
+$servername = "localhost:3306"; // Ou l'adresse de votre serveur SQL
 $username = "cycalguj";
 $password = "CYCalender1234";
 $dbname = "CYCalenderB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Vérification de la connexion
 if ($conn->connect_error) {
-    die("Échec de la connexion: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$data = json_decode(file_get_contents("php://input"), true);
-
+// Démarrer la session
 session_start();
 
-foreach ($data as $event_data) {
-    $day = intval($event_data['day']); // Convertir en entier
-    $month = intval($event_data['month']);
-    $year = intval($event_data['year']);
-    $events = $event_data['events'];
+// Vérifier si l'ID de session existe
+if(isset($_SESSION['user_id'])) {
+    // Construire un tableau associatif contenant les données de l'utilisateur
+    $userData = array(
+        'user_id' => $_SESSION['user_id'],
+        'first_name' => $_SESSION['first_name'],
+        'last_name' => $_SESSION['last_name'],
+        'username' => $_SESSION['username']
+    );
 
-    foreach ($events as $event) {
-        $title = $event['title'];
-        $time = $event['time'];
-        $event_time = date("H:i", strtotime($time));
-        $description = isset($event['description']) ? $event['description'] : '';
-        $place = isset($event['place']) ? $event['place'] : '';
-        $creator = isset($_SESSION['username']) ? $_SESSION['username'] : ''; // Vérifier si la clé 'username' est définie
-        $code_agenda = isset($_SESSION['agenda_code']) ? $_SESSION['agenda_code'] : ''; // Vérifier si la clé 'agenda_code' est définie
+    // Définir agenda_code sur 0
+    $userData['agenda_code'] = 0;
 
-        $sql_insert = "INSERT INTO events (day, month, year, title, event_time, description, place, creator, code_agenda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bind_param("iiissssss", $day, $month, $year, $title, $event_time, $description, $place, $creator, $code_agenda);
-        $stmt->execute();
-    }
+    // Convertir le tableau en format JSON et le renvoyer
+    header('Content-Type: application/json');
+    echo json_encode($userData);
+} else {
+    echo json_encode(array("error" => "Session utilisateur non trouvée."));
 }
-
-$conn->close();
-
-echo json_encode(array("message" => "Événements enregistrés avec succès"));
 ?>
+
 
