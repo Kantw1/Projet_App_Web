111ea503f1605a886e55ba031a5fbd4ec8f24a24
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-13 16:13:02 +0200

    problème de connection session rectifié9

--- window_onload.js
+++ window_onload.js
@@ -1,9 +1,9 @@
 // Appel de la fonction pour afficher les données des agendas au chargement de la page
 window.onload = function() {
-    getUserData();
-    getAgendaData();
-    getAgendaData2();
-    
-    
-
+    Promise.all([getUserData(), getAgendaData(), getAgendaData2()])
+    .catch(error => {
+        console.error('Une erreur s\'est produite lors du chargement des données:', error);
+        // Redirection vers index.html en cas d'erreur
+        window.location.href = 'index.html';
+    });
 };
