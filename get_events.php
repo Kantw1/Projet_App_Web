e289bae1d032e5501f56ae434fbcdfc191de4712
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-15 18:38:50 +0200

    Update get_events.php

--- get_events.php
+++ get_events.php
@@ -14,40 +14,22 @@ try {
 
 // Vérifie si l'utilisateur est connecté
 session_start();
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['agenda_code'])) {
     die("Vous n'êtes pas connecté.");
 }
 
-// Récupérer l'agenda personnel de l'utilisateur
-$query_agenda_perso = "SELECT agenda_code FROM user_agenda WHERE user_id = :user_id AND agenda_perso_code = :agenda_perso_code";
-$stmt_agenda_perso = $pdo->prepare($query_agenda_perso);
-$stmt_agenda_perso->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
-$stmt_agenda_perso->bindParam(':agenda_perso_code', $_SESSION['agenda_perso_code'], PDO::PARAM_STR);
-$stmt_agenda_perso->execute();
-$agenda_perso = $stmt_agenda_perso->fetch(PDO::FETCH_ASSOC);
+$agenda_code = $_SESSION['agenda_code'];
 
-if ($agenda_perso) {
-    // Si l'utilisateur a un agenda personnel, récupérer tous les événements de tous les agendas
-    $query_events = "SELECT * FROM events WHERE code_agenda IN (SELECT agenda_code FROM user_agenda WHERE user_id = :user_id)";
-    $stmt_events = $pdo->prepare($query_events);
-    $stmt_events->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
-} else {
-    // Sinon, récupérer uniquement les événements de l'agenda spécifié
-    if (!isset($_SESSION['agenda_code'])) {
-        die("Aucun agenda sélectionné.");
-    }
-    $agenda_code = $_SESSION['agenda_code'];
-    $query_events = "SELECT * FROM events WHERE code_agenda = :code_agenda";
-    $stmt_events = $pdo->prepare($query_events);
-    $stmt_events->bindParam(':code_agenda', $agenda_code, PDO::PARAM_STR);
-}
-
-$stmt_events->execute();
+// Préparation de la requête
+$query = "SELECT * FROM events WHERE code_agenda = :code_agenda";
+$stmt = $pdo->prepare($query);
+$stmt->bindParam(':code_agenda', $agenda_code, PDO::PARAM_STR);
+$stmt->execute();
 
 $eventsArr = [];
 
 // Récupération des résultats
-while ($row = $stmt_events->fetch(PDO::FETCH_ASSOC)) {
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $event = [
         'day' => (int)$row['day'],
         'month' => (int)$row['month'],
