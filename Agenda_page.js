f13bc206e759b56188fab02500fc796964979715
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-15 19:54:21 +0200

    Update Agenda_page.js

--- Agenda_page.js
+++ Agenda_page.js
@@ -568,7 +568,7 @@ eventsContainer.addEventListener("click", (e) => {
     }
 });
 
-
+/*
 //function to get events from local storage
 function getEvents() {
   // Vide complètement eventsArr avant de le remplir avec les nouveaux événements
@@ -593,7 +593,7 @@ function getEvents() {
     console.error('Erreur lors de la récupération des événements:', error);
   });
 }
-
+/*/
 
 function saveEvents() {
   console.log('Données transmises :', JSON.stringify(eventsArr));
