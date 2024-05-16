3a1ec4a419b795ef1cf75d1048ba5c5b1a135046
Quentin Fourrier <fourrierquentin9@gmail.com>
2024-05-16 16:35:48 +0200

    ajout agenda ma13

--- Add_agenda.js
+++ Add_agenda.js
@@ -19,9 +19,24 @@ Add_agenda.addEventListener("click", () => {
         if(data.message) {
             alert(data.message); // Afficher le message de réponse
         } else {
-            getAgendaData_ajout();
-            var nav = document.querySelector('.new-agenda');
-            nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
+            // Effectuer la deuxième requête uniquement si la première réussit
+            fetch('connection_agenda_user.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Traiter la réponse de la deuxième requête ici si nécessaire
+                // Dans votre cas, vous pouvez appeler getAgendaData_ajout() ici
+                getAgendaData_ajout();
+                
+                // Afficher ou masquer l'élément .new-agenda
+                var nav = document.querySelector('.new-agenda');
+                nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
+            })
+            .catch(error => console.error('Erreur lors de la connexion de l\'agenda à l\'utilisateur:', error));
         }
     })
     .catch(error => console.error('Erreur lors de la création de l\'agenda:', error));
