c4a834489365e8f5d0ba604b1ad9a11d5ba14490
Robardbapt <131241214+Robardbapt@users.noreply.github.com>
2024-05-17 11:52:40 +0200

    Update create_event.php

--- create_event.php
+++ create_event.php
@@ -44,7 +44,7 @@ foreach ($data as $event_data) {
         $creator = isset($_SESSION['username']) ? $_SESSION['username'] : '';
 
         // Vérifier si l'événement existe déjà
-        $sql_check = "SELECT * FROM events WHERE day = ? AND month = ? AND year = ? AND title = ? AND event_time = ? AND description = ? AND place = ?";
+        $sql_check = "SELECT * FROM events WHERE day = ? AND month = ? AND year = ? AND title = ? AND event_time = ? AND description = ? AND place = ? AND code_agenda = ?";
         $stmt_check = $conn->prepare($sql_check);
         $stmt_check->bind_param("iiisssss", $day, $month, $year, $title, $event_time, $description, $place, $code_agenda);
         $stmt_check->execute();
